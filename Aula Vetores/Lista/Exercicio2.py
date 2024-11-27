06a4fe2fef306673f2cdd4abb0604f63340e46cf
Mauricio Bertuci Saletti <bertuci.mauricio@gmail.com>
2024-11-27 14:52:24 -0300

    Delete Aula Vetores directory

--- Aula Vetores/Lista/Exercicio2.py
+++
@@ -1,9 +0,0 @@
-#Faça um programa que leia um vetor de 10 numeros reias e mostre-os
-
-valor = []
-num = 0 
-while num < 10:
-    valor.append(float(input("Digite um numero: ")))
-    num +=1
-valor.reverse()
-print(valor)
