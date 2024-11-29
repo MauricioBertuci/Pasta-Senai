60b064da40190fd7502924e4722b98d3a52454b3
Mauricio Bertuci Saletti <bertuci.mauricio@gmail.com>
2024-11-29 11:12:36 -0300

    terminando medias dos alunos

--- Aula Vetor e Matriz/Matriz/Exercicio1.py
+++ Aula Vetor e Matriz/Matriz/Exercicio1.py
@@ -1,22 +1,32 @@
 #Faça um programa que peça as quatro notas de seis alunos, calcule e armazene num vetor a media de cada aluno, 
 # imprima o numero de alunos com media maior ou igual a 7
 
+#Pedindo notas
 print("Preencha as notas fos alunos ")
 matriz= []
-
-
-for i in range(6):
+for aluno in range(2):
+    aluno= input("Digite o nome do aluno: ")
+    # nome = [] #verificarr
     linha= []
-    for coluna in range(4):
-        nota= int(input("Digite a nota do aluno: ")) #Pede a nota dos alunos
-        linha.append(nota) #adiciona nota de cada aluno
-    matriz.append(linha) #cria linha e colunas
+    for coluna in range(2):
+        valor= float(input(f"Digite a nota {coluna+1} do aluno {aluno}: ")) 
+        linha.append(valor) 
+    matriz.append(linha) 
+
 
 print(f"Tabela das notas dos alunos:  {matriz} ")
 
+#caucular media
+medias_aluno = []
+for linha in matriz:
+    media = sum(linha) / len(linha)
+    # nome.append(aluno) ##verificarrrrrrr
+    medias_aluno.append(media) 
+print(f"Media dos alunos: {medias_aluno}") 
 
-print()
-print()
-    
-media = sum(linha) / len(linha)
-print(media)
+#Ver notas
+for media in medias_aluno:
+    if media >= 7:
+        print(f"O aluno Passou !!")
+    else:
+        print(f"O aluno Reprovou !!")
